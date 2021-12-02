6bdbad6a80eaedd0f4459a25c88eeb42c74fd08e
Ilyas Bezhanidze <iljas.bezhanidze@totonis.com>
2021-12-02 18:45:24 +0200

    slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,6 @@
 const menuItems = document.querySelectorAll('.b-menu__list-item')
 const burgerButton = document.querySelector('.b-burger')
 const header = document.querySelector('.b-header')
-const impositionBlocks = document.querySelectorAll('.b-imposition')
-const impositionTextInner = document.querySelectorAll('.b-imposition__inner')
-const impositionTopImage = document.querySelectorAll('.b-imposition__main-img')
-
 
 //menu toggler
 burgerButton.onclick = () => header.classList.toggle('b-toggle')
@@ -12,12 +8,14 @@ burgerButton.onclick = () => header.classList.toggle('b-toggle')
 
 //watching for menu 
 const observer = new MutationObserver(containClass)
-observer.observe(header, {'attributes': true})
+observer.observe(header, {
+  'attributes': true
+})
 
 
 //block scroll body
 function containClass() {
-  if(header.classList.contains('b-toggle')) document.body.style.overflow = 'hidden'
+  if (header.classList.contains('b-toggle')) document.body.style.overflow = 'hidden'
   else document.body.style.overflow = 'auto'
 }
 
@@ -29,12 +27,28 @@ for (let i = 0, amount = 0; i < menuItems.length; i++, amount += 30) {
 
 
 //slider (different size pictures)
-var mySwiper = new Swiper(".mySwiper_team", {slidesPerView: 'auto'});
+var mySwiper = new Swiper(".mySwiper_team", {
+  slidesPerView: 'auto'
+});
 
 
 //init & destroy slider (different size pictures) depending on viewport width
 window.innerWidth < 800 ? mySwiper() : mySwiper.destroy()
 
 
-
+//init slider (width articles)
+var swiperBlog = new Swiper(".mySwiperBlog", {
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+  loop: true,
+  breakpoints: {
+    800: {
+      spaceBetween: 40,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
