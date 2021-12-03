6dba4e5d74ebeacde1c51c6c0df4d929a85fc9c9
Ilyas Bezhanidze <iljas.bezhanidze@totonis.com>
2021-12-03 19:51:39 +0200

    pages: article? news? cases

--- src/js/main.js
+++ src/js/main.js
@@ -25,6 +25,36 @@ for (let i = 0, amount = 0; i < menuItems.length; i++, amount += 30) {
   menuItems[i].style.paddingLeft = `${amount}px`
 }
 
+
+//VIDEO
+const videos = document.querySelectorAll('.b-video-article');
+let generateUrl = function(id) {
+	let query = '?rel=0&showinfo=0&autoplay=1';
+	return 'https://www.youtube.com/embed/' + id + query;
+};
+let createIframe = function(id) {
+	let iframe = document.createElement('iframe');
+	iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('allow', 'autoplay; encrypted-media');
+	iframe.setAttribute('src', generateUrl(id));
+	return iframe;
+};
+videos.forEach((el) => {
+	let videoHref = el.getAttribute('data-video');
+	let deletedLength = 'https://youtu.be/'.length;  //заполнить индивидуально
+	let videoId = videoHref.substring(deletedLength, videoHref.length);
+	let img = el.querySelector('img');
+	let youtubeImgSrc = 'https://i.ytimg.com/vi/' + videoId + '/maxresdefault.jpg';
+	img.setAttribute('src', youtubeImgSrc);
+	el.addEventListener('click', (e) => {
+		e.preventDefault();
+		let iframe = createIframe(videoId);
+		el.querySelector('img').remove();
+		el.appendChild(iframe);
+		el.querySelector('button').remove();
+	});
+});
+
 //SLIDERS//
 //init slider (width articles)
 var swiperBlog = new Swiper(".mySwiperBlog", {
@@ -34,6 +64,21 @@ var swiperBlog = new Swiper(".mySwiperBlog", {
   breakpoints: {
     800: {
       spaceBetween: 40,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiperArticle = new Swiper(".mySwiperArticle", {
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+  loop: true,
+  breakpoints: {
+    800: {
+      spaceBetween: 60,
     },
   },
   navigation: {
